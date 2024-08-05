a79aefa6580869f99af654584b9923e78d14112e
Mehul Mohan <mehulmpt@gmail.com>
2024-08-05 19:17:35 +0530

    Create MyFile.java

---
+++ MyFile.java
@@ -0,0 +1,12 @@
+public class MyFile {
+    public static void main(String []args){
+
+        System.out.println("HKM");
+    }
+}
+
+
+// First boot -> github
+// Second time and onwards => Subsequent boots -> are from user's own work from past
+
+// 
